18edcda74aaffff59363f99be4f81c660274ab84
Thomas Dürr <thomduerr@gmail.com>
2024-04-15 23:36:43 +0200

    api routes

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,17 @@
 # Notebook Backend
 
 ## Development
+
+    python -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+    pip install ipython
+
+    export REMOTE_PAGES_REPO="$HOME/.gitrepos/wiki-pages.git"
+    export LOCAL_PAGES_REPO=".pages"
+    export PAGES_DIR=".pages"
+    uvicorn app.main:app --reload
+
+## Routes
+
+    curl -s localhost:8000/pages | jq
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,15 @@ from typing import Union
 from fastapi import FastAPI
 from git import Repo
 import os
+import logging
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO, format='[API]: %(message)s')
 
 remote_pages_repo = os.getenv("REMOTE_PAGES_REPO")
 local_pages_repo = os.getenv("LOCAL_PAGES_REPO")
+pages_dir = os.getenv("PAGES_DIR")
+
 try:
     repo = Repo(local_pages_repo)
     repo.remotes.origin.pull()
@@ -12,17 +18,18 @@ except:
     repo = Repo.clone_from(remote_pages_repo, local_pages_repo)
 
 status = repo.git.status()
-print(status)
-
+logging.info(status)
 
 app = FastAPI()
 
+@app.get("/pages")
+def read_pages():
+    path = Path(pages_dir)
+    pages = [(p.name, p.stem) for p in path.iterdir() if p.is_file()]
+    return {"pages": [{"filename": page[0], "id": page[1], "name": page[1].title()} for page in pages]}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/pages/{page_id}")
+def read_item(page_id: str):
+    path = Path(f'{pages_dir}/{page_id}.md')
+    content = path.read_text(encoding="utf-8")
+    return {"page": {"filename": path.name, "id": path.stem, "name": path.stem.title()}, "content": content}

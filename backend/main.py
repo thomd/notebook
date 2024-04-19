c6831eaa06cdeb7b30a49f8a45722a585c525422
Thomas Dürr <thomduerr@gmail.com>
2024-04-19 21:23:01 +0200

    api

--- backend/main.py
+++ backend/main.py
@@ -9,15 +9,13 @@ app = FastAPI()
 
 @app.get("/pages")
 def read_pages():
-    path = Path(pages_dir)
-    pages = [(p.name, p.stem) for p in path.iterdir() if p.is_file()]
-    return {"pages": [{"filename": p[0], "id": p[1], "name": p[1].title()} for p in pages]}
+    pages = []
+    for path in Path(pages_dir).iterdir():
+        if path.is_file():
+            pages.append(page.create(path))
+    return pages
 
 @app.get("/pages/{page_id}")
 def read_item(page_id: str):
     path = Path(f'{pages_dir}/{page_id}.md')
-    filename = page.getFilename(path)
-    id = page.getId(path)
-    title = page.getTitle(path)
-    content = page.getContent(path)
-    return model.Page(id, filename, title, content)
+    return page.create(path)
--- backend/notebook/page.py
+++ backend/notebook/page.py
@@ -1,13 +1,16 @@
+from notebook import model
 
-def getFilename(path):
+def _filename(path):
     return path.name
 
-def getId(path):
+def _id(path):
     return path.stem
 
-def getTitle(path):
+def _title(path):
     return path.stem.title()
 
-def getContent(path):
+def _content(path):
     return path.read_text(encoding="utf-8")
 
+def create(path):
+    return model.Page(_id(path), _filename(path), _title(path), _content(path))

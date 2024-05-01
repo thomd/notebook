3417385cf9c953ae05c4b45236e1693f79b69b96
Thomas Dürr <thomduerr@gmail.com>
2024-05-01 23:46:43 +0200

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,12 @@
-import React, { useState, useRef } from "react"
-import { ChakraProvider, Heading, useDisclosure, ModalOverlay, Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, FormControl, Input } from '@chakra-ui/react'
-import { useHotkeys } from 'react-hotkeys-hook'
+import React from "react"
+import { ChakraProvider, Heading } from '@chakra-ui/react'
+import Search from './components/Search'
 
 function App() {
-  const SearchOverlay = () => (
-    <ModalOverlay
-      bg='blackAlpha.300'
-      backdropFilter='blur(10px) hue-rotate(90deg)'
-    />
-  )
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const initialRef = useRef(null)
-  const [search, openSearch] = useState(<SearchOverlay />)
-  useHotkeys('shift+/', () => {
-    openSearch(<SearchOverlay />)
-    onOpen()
-  })
   return (
     <ChakraProvider>
       <Heading>I'm a Heading</Heading>
-      <Modal isCentered initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
-        {search}
-        <ModalContent>
-          <ModalHeader>Search</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <FormControl>
-              <Input ref={initialRef} placeholder='Search' />
-            </FormControl>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <Search />
     </ChakraProvider>
   )
 }
---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useRef } from "react"
+import { useDisclosure, ModalOverlay, Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, FormControl, Input } from '@chakra-ui/react'
+import { useHotkeys } from 'react-hotkeys-hook'
+
+const Search = () => {
+  const SearchOverlay = () => (
+    <ModalOverlay
+      bg='blackAlpha.300'
+      backdropFilter='blur(10px) hue-rotate(90deg)'
+    />
+  )
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = useRef(null)
+  const [search, openSearch] = useState(<SearchOverlay />)
+  useHotkeys('shift+/', () => {
+    openSearch(<SearchOverlay />)
+    onOpen()
+  })
+  return (
+    <Modal isCentered initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+      {search}
+      <ModalContent>
+        <ModalHeader>Search</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormControl>
+            <Input ref={initialRef} placeholder='Search' />
+          </FormControl>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default Search;
+

88c7cf3040f35aed3fa7915d2d72f4c6042fa1b6
Thomas Dürr <thomduerr@gmail.com>
2024-05-01 11:10:19 +0200

    backend

--- backend/main.py
+++ backend/main.py
@@ -8,14 +8,14 @@ pages_dir = os.getenv("PAGES_DIR")
 app = FastAPI()
 
 
-# curl http://127.0.0.1:8000/pages -s | jq
+# curl -H "Content-Type: application/json " http://localhost:8000/pages -s | jq
 @app.get("/pages", response_model=model.Pages, response_model_exclude_none=True)
 def get_pages():
     path = Path(pages_dir)
     return pg.getPages(path)
 
 
-# curl http://127.0.0.1:8000/pages/foo -s | jq
+# curl -H "Content-Type: application/json" http://localhost:8000/pages/foo -s | jq
 @app.get("/pages/{page_id}", response_model=model.Response, response_model_exclude_none=True)
 def get_page(page_id: str):
     path = Path(f"{pages_dir}/{page_id}.md")
@@ -25,7 +25,7 @@ def get_page(page_id: str):
         raise HTTPException(status_code=404, detail=f"Page '{page_id}' does not exist")
 
 
-# curl -H "Content-Type: application/json" -d '{"title": "Foo", "content": "# Foo"}' http://127.0.0.1:8000/pages -s | jq
+# curl -H "Content-Type: application/json" -d '{"title": "Foo", "content": "# Foo"}' http://localhost:8000/pages -s | jq
 @app.post("/pages", status_code=201, response_model=model.Response, response_model_exclude_none=True)
 def create_page(page: model.Page):
     filename = pg.createFilename(page.title)
@@ -36,7 +36,7 @@ def create_page(page: model.Page):
         return pg.createPage(path, page)
 
 
-# curl -X DELETE http://127.0.0.1:8000/pages/foo -s | jq
+# curl -H "Content-Type: application/json" -X DELETE http://localhost:8000/pages/foo -s | jq
 @app.delete("/pages/{page_id}")
 def delete_page(page_id: str):
     path = Path(f"{pages_dir}/{page_id}.md")
@@ -47,7 +47,7 @@ def delete_page(page_id: str):
         raise HTTPException(status_code=404, detail=f"Page '{page_id}' does not exist")
 
 
-# curl -H "Content-Type: application/json" -X PUT -d '{"title": "Foo", "content": "# Foo"}' http://127.0.0.1:8000/pages/foo -s | jq
+# curl -H "Content-Type: application/json" -X PUT -d '{"title": "Foo", "content": "# Foo"}' http://localhost:8000/pages/foo -s | jq
 @app.put("/pages/{page_id}", response_model=model.Response, response_model_exclude_none=True)
 def update_page(page_id: str, page: model.Page):
     """ PUT is used to replace the existing page """
@@ -60,9 +60,9 @@ def update_page(page_id: str, page: model.Page):
         return pg.putPage(path, page)
 
 
-# curl -H "Content-Type: application/json" -X PATCH -d '{"title": "Other Foo"}' http://127.0.0.1:8000/pages/foo -s | jq
+# curl -H "Content-Type: application/json" -X PATCH -d '{"title": "Other Foo"}' http://localhost:8000/pages/foo -s | jq
 @app.patch("/pages/{page_id}", response_model=model.Response, response_model_exclude_none=True)
-def change_page(page_id: str, page: model.PageUpdate):
+def change_page_attributes(page_id: str, page: model.PageUpdate):
     """ PATCH is used to partially replace existing page attributes """
     path = Path(f'{pages_dir}/{page_id}.md')
     if page.title != None and Path(f'{pages_dir}/{pg.createFilename(page.title)}').exists() and not path.exists():

9a3ca5bcd4036497e7875b36f72523ccb5b34811
Thomas Dürr <thomduerr@gmail.com>
2024-06-17 12:09:46 +0200

    frontend

--- frontend/src/components/Markdown.jsx
+++ frontend/src/components/Markdown.jsx
@@ -3,12 +3,17 @@ import remarkGfm from 'remark-gfm'
 import rehypeKatex from 'rehype-katex'
 import rehypeSlug from 'rehype-slug'
 import remarkMath from 'remark-math'
+import rehypeRaw from 'rehype-raw'
 import 'katex/dist/katex.min.css'
 
 export default function MarkdownViewer({ content }) {
   return (
     <div className="markdown">
-      <Markdown remarkPlugins={[remarkGfm, remarkMath]} rehypePlugins={[rehypeKatex, rehypeSlug]}>{content}</Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeKatex, rehypeSlug, rehypeRaw]}
+        remarkRehypeOptions={{allowDangerousHtml: true}}
+      >{content}</Markdown>
     </div>
   )
 }

6b413d0515e49eea312b45f63ea835d53d33bdd3
Thomas Dürr <thomduerr@gmail.com>
2024-07-30 00:15:17 +0200

    frontend

--- frontend/src/components/Favorites.jsx
+++ frontend/src/components/Favorites.jsx
@@ -2,18 +2,23 @@ import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
 
 export function FavoritesMenu({ pages, className }) {
   const favoritePages = pages.filter((page) => page.favorite === true)
+
   return (
-    <div className={`${className || ''} inline`}>
-      <Menu offset={[-10, 10]} autoSelect={false}>
-        <MenuButton className="text-lg text-gray-400 hover:text-gray-500">Favorites</MenuButton>
-        <MenuList>
-          {favoritePages.map((page) => (
-            <MenuItem className="hover:bg-white hover:text-gray-500" key={page.id} as="a" href={`/pages/${page.id}/`}>
-              {page.title}
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
-    </div>
+    <>
+      {favoritePages.length > 0 && (
+        <div className={`${className || ''} inline`}>
+          <Menu offset={[-10, 10]} autoSelect={false}>
+            <MenuButton className="text-lg text-gray-400 hover:text-gray-500">Favorites</MenuButton>
+            <MenuList>
+              {favoritePages.map((page) => (
+                <MenuItem className="!py-1 hover:bg-white !text-gray-400 hover:!text-gray-600" key={page.id} as="a" href={`/pages/${page.id}/`}>
+                  {page.title}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        </div>
+      )}
+    </>
   )
 }

5a49e28204245db63e5fae8907d0222cdf0488f8
Thomas Dürr <thomduerr@gmail.com>
2024-09-22 22:05:30 +0200

    backend

---
+++ backend/init/clone_git.py
@@ -0,0 +1,23 @@
+from git import Repo
+from pathlib import Path
+from notebook import page as pg
+from notebook import log
+import os
+
+REMOTE_PAGES_REPO = os.environ.get('REMOTE_PAGES_REPO')
+LOCAL_PAGES_REPO = os.environ.get('LOCAL_PAGES_REPO')
+
+if REMOTE_PAGES_REPO and LOCAL_PAGES_REPO:
+    try:
+        repo = Repo(LOCAL_PAGES_REPO, search_parent_directories=True)
+        repo.remotes.origin.pull()
+        log.info(f"git pull into '{LOCAL_PAGES_REPO}'")
+    except:
+        repo = Repo.clone_from(REMOTE_PAGES_REPO, LOCAL_PAGES_REPO)
+        log.info(f"cloning '{REMOTE_PAGES_REPO}' to '{LOCAL_PAGES_REPO}'")
+    finally:
+        repo.config_writer().set_value("user", "name", "Thomas Dürr").release()
+        repo.config_writer().set_value("user", "email", "thomduerr@gmail.com").release()
+        log.info(f'git status: {repo.git.status()}')
+else:
+    log.error('Environment not set')
---
+++ backend/init/index_elasticsearch.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from elasticsearch import Elasticsearch
+from notebook import page as pg
+from notebook import log
+
+es = Elasticsearch('http://localhost:9200')
+
+if not es.indices.exists(index='notebooks'):
+    es.indices.create(index='notebooks', body={})
+    log.info(f"created ealsticesearch index 'notebooks'")
+    path = Path(pg.pagesDir())
+    pagecount = 0
+    for page in path.iterdir():
+        if page.is_file():
+            page = pg.getPage(page)
+            pagecount += 1
+            document = {
+                "title": page.title,
+                "category": page.category,
+                "content": page.content
+            }
+            result = es.index(index='notebooks', body=document)
+            log.info(f"ingested document '{page.title}' ({result['_id']})")
+    log.info(f'ingested {pagecount} pages into elasticsearch')
